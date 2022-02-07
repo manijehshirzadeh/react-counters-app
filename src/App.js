2f114513606b687a8823fabf174d355d0f748d70
manijehshirzadeh <manijeh.shirzadeh63@gmail.com>
2022-02-07 18:32:50 +1100

    created 4 counters, used bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./Counter";
+import { useState } from "react";
+
+const initialNumber = [0, 0, 0, 0];
 
 function App() {
+  const [counters, setCounters] = useState(initialNumber);
+  // console.log(counters);
+  const handleAdd = (index) => {
+    const newCounters = [...counters];
+    newCounters[index] = newCounters[index] + 1;
+
+    setCounters(newCounters);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+          Navbar
+          <button type="button" className="btn btn-secondary">
+            {0}
+          </button>
+        </div>
+        <div className="d-grid gap-2 col-1 mx-auto">
+          <button type="button" className="btn btn-primary">
+            Reset
+          </button>
+        </div>
+      </div>
+      {counters.map((counter, index) => {
+        return <Counter number={counter} onAdd={() => handleAdd(index)} />;
+      })}
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function Counter(props) {
+  return (
+    <div
+      className="d-inline-flex p-2 bd-highlight"
+      className="d-flex justify-content-center"
+    >
+      <button
+        className="btn btn-warning"
+        type="button"
+        style={{ marginRight: "35px" }}
+      >
+        {props.number}
+      </button>
+      <div className="d-grid gap-2 d-md-block">
+        <button
+          className="btn btn-secondary"
+          type="button"
+          onClick={props.onAdd}
+        >
+          +
+        </button>
+        <button className="btn btn-secondary" type="button">
+          _
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-md-block">
+        <button
+          className="btn btn-danger"
+          type="button"
+          style={{ marginLeft: "35px" }}
+        >
+          delete
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Counter;
